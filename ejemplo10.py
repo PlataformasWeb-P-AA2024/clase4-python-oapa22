333a6410ae2aaa76ff7d3bcb08dec254b7ecbe3a
Oliver <oliver2002@Ubuntu.myguest.virtualbox.org>
2024-04-30 02:26:44 -0500

    Archivos

--- ejemplo10.py
+++ ejemplo10.py
@@ -3,10 +3,30 @@ archivo = open("data/atp_tennis.csv", "r")
 lineas = archivo.readlines()
 lineas = [l.split("|") for l in lineas]
 
-for x in lineas:
-    cadena = """<b>Torneo:</b> %s <br> <b>\nGanador:</b> %s""" % (x[0], x[9])
-    print(cadena)
+# Versión con los 25363 archivos:
 
-    archivo_generado = open("data/%s" % (x[9]), "w")
-    archivo_generado.writelines("%s\n" % (cadena))
-    archivo_generado.close()
+# contador = 1
+# for x in lineas:
+#         cadena = """<b>Torneo:</b> %s <br> <b>\nGanador:</b> %s""" % (x[0], x[9])
+#         print(cadena)
+#
+#         archivo_generado = open("data/ArchivoN_%s" % contador, "w")
+#         archivo_generado.writelines("%s\n" % (cadena))
+#         archivo_generado.close()
+#         contador += 1
+
+# Versión con límite de 100 para que no me explote la computadora:
+
+contador = 1
+bol = False
+for x in lineas:
+    if not bol:
+        cadena = """<b>Torneo:</b> %s <br> <b>\nGanador:</b> %s""" % (x[0], x[9])
+        print(cadena)
+
+        archivo_generado = open("data/ArchivoN_%s" % contador, "w")
+        archivo_generado.writelines("%s\n" % (cadena))
+        archivo_generado.close()
+        contador += 1
+        if contador > 100:
+            bol = True
